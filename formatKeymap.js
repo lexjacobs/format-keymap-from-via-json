111ea9c14304a100eb8db19bbcd5c467b60ca6a0
Alex Jacobs <lex.jacobs@gmail.com>
2022-03-28 23:37:39 -0700

    Print each layer by rows divided by text delimiter
    Need to:
    * format
    * add some spacing between halves
    * add in layer headers

--- formatKeymap.js
+++ formatKeymap.js
@@ -14,21 +14,23 @@ const writeKeymaps = (data) => {
 
 const makeRows = layers => {
   let rows = R.map(layer => {
+    // divide the layer array in half for left/right halves so right can be reversed
     let [left, right] = R.splitAt(R.divide(R.length(layer), 2), layer)
-    // for left, bucket into groups of 6
+    // for left, bucket into groups of 6 for each keyboard row on that half
     left = R.splitEvery(6, left);
-    // for right, same, but also reverse each bucket
+    // for right, same, but also reverse each bucket of keys
     right = R.map(R.reverse, R.splitEvery(6, right));
+    // then a full row of keys is a zipping of left and right
     let row = R.map(R.flatten, R.zip(left, right));
-    // then a row is a zipping of left and right
-    console.log({row})
-    return row;
+    // split in half and add some space
+    // console.log(row)
+    return row.join('\n//|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------|\n');
     // then add delimiters
     // join with newlines
     // and return
     // for gergoplex keymap conversion, remove the first and last keys
   })(layers)
-  console.log({rows});
+  console.log(rows.join('\n\n\n'));
 }
 
 
